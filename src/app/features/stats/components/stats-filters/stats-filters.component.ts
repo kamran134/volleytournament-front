90323c116995c2b41be80ae11c30f4aa3be118fe
Kamran Kazimi <kazimi.msu@gmail.com>
2025-05-04 21:45:26 +0400

    last changes for 3 and 5

--- src/app/features/stats/components/stats-filters/stats-filters.component.ts
+++ src/app/features/stats/components/stats-filters/stats-filters.component.ts
@@ -46,7 +46,6 @@ import { MonthYearPipe } from '../../../../shared/pipes/month-year.pipe';
         MatSortModule,
         CommonModule,
         ReactiveFormsModule,
-        MonthYearPipe,
         RouterModule,
         FormsModule
     ],
