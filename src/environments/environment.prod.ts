58ca0a78b7a907cb49cc5d46464803a85083db48
Kamran Kazimi <kazimi.msu@gmail.com>
2025-05-20 23:51:42 +0400

    prod edited

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://157.230.29.19:5000/api',
-    authUrl: 'http://157.230.29.19:5000/auth'
+    apiUrl: '/api',
+    authUrl: '/auth'
 };
